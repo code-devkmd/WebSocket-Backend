49a8e6f8860fa289ad699d657e2d07130c19b039
code-devkmd <nandeshoreningombam@gmail.com>
2026-06-18 17:36:51 +0530

    Fixed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://anonymous-chat-chi.vercel.app/", 
+        origin: "https://anonymous-chat-chi.vercel.app", 
         methods: ["GET", "POST"]
     }
 });
